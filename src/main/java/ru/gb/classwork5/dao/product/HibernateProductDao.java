52e90f1e63341b43f481cf670c74aef4008e187a
Vitaly <vitalykri123@gmail.com>
2022-02-04 20:03:47 +0500

    add homework5

---
+++ src/main/java/ru/gb/classwork5/dao/product/HibernateProductDao.java
@@ -0,0 +1,30 @@
+package ru.gb.classwork5.dao.product;
+
+import lombok.RequiredArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.gb.classwork5.entity.Product;
+
+import java.util.Collections;
+
+@Repository
+@RequiredArgsConstructor
+public class HibernateProductDao implements ProductDao {
+    private final SessionFactory sessionFactory;
+
+    @Override
+    @Transactional(readOnly = true)
+    public Iterable<Product> findAll() {
+        return Collections.unmodifiableList(sessionFactory.getCurrentSession().getNamedQuery("Product.getAll").list());
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Product findById(Long id) {
+        return (Product) sessionFactory.getCurrentSession()
+                .getNamedQuery("Product.findById")
+                .setParameter("id", id)
+                .getSingleResult();
+    }
+}
---
+++ src/main/java/ru/gb/classwork5/dao/product/SpringJDBCProductDao.java
@@ -0,0 +1,63 @@
+package ru.gb.classwork5.dao.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import ru.gb.classwork5.entity.Manufacturer;
+import ru.gb.classwork5.entity.Product;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+//@Component
+@RequiredArgsConstructor
+public class SpringJDBCProductDao implements ProductDao{
+
+
+    private final DataSource dataSource;
+
+
+
+    @Override
+    public Iterable<Product> findAll() {
+        return null;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        Connection connection = null;
+        Product product = null;
+        try {
+            connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM product where id = ?");
+            preparedStatement.setLong(1,id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                product = Product.builder()
+                        .cost(resultSet.getBigDecimal("cost"))
+                        .title(resultSet.getString("title"))
+                        .date(resultSet.getDate("manufacture_date").toLocalDate())
+                        .id(resultSet.getLong("id"))
+                        .build();
+                break;
+            }
+            preparedStatement.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            return product;
+        }
+
+    }
+}
